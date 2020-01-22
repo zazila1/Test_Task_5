dadd89c1eed1200c6f12e8056ade799e42608c47
zazila1 <zazila1@gmail.com>
2020-01-22 03:56:07 +0300

    Стрельба из оружия с нужной задержкой, эффект стрельбы

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,9 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private ParticleSystem _ShootEffect;
+
+    private bool canShoot = true;
+
+    private float shootDelay = 0.1f;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -11,8 +18,27 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetAxisRaw("Fire1") > 0)
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        if (canShoot)
+        {
+            _ShootEffect.Play();
+            StartCoroutine(ShootingDelay(shootDelay));
+        }    
+    }
+
+    private IEnumerator ShootingDelay(float delay)
+    {
+        canShoot = false;
+        yield return new WaitForSeconds(delay);
+        canShoot = true;
     }
 }
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Weapon", menuName = "Weapon")]
+public class Weapon : ScriptableObject
+{
+    public string _Name;
+    public Sprite _Icon;
+    public ParticleSystem _ShootEffect;
+}
