baedb8f8c4694843a3701efd9fe2eb17f031aece
zazila1 <zazila1@gmail.com>
2020-01-22 07:06:30 +0300

    Отрисовка радуса спавнера в редакторе

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.PlayerLoop;
 
 public class Spawner : MonoBehaviour
 {
     [SerializeField] private PlayerController _Player;
     [SerializeField] private GameObject _EnemyPrefab;
-
+    public float _SpawnRadius;
+    
+    
 }
---
+++ Assets/Scripts/SpawnerEditorHelper.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class SpawnerEditorHelper : MonoBehaviour
+{
+    private Spawner _Spawner;
+
+    private void Start()
+    {
+        _Spawner = GetComponent<Spawner>();
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.white;
+        float theta = 0;
+        float x = _Spawner._SpawnRadius * Mathf.Cos(theta);
+        float y = _Spawner._SpawnRadius * Mathf.Sin(theta);
+        Vector3 pos = transform.position + new Vector3(x, 0, y);
+        Vector3 newPos = pos;
+        Vector3 lastPos = pos;
+        for (theta = 0.1f; theta < Mathf.PI * 2; theta += 0.1f)
+        {
+            x = _Spawner._SpawnRadius * Mathf.Cos(theta);
+            y = _Spawner._SpawnRadius * Mathf.Sin(theta);
+            newPos = transform.position + new Vector3(x, y, 0);
+            Gizmos.DrawLine(pos, newPos);
+            pos = newPos;
+        }
+        Gizmos.DrawLine(pos, lastPos);
+    }
+}
