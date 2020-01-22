ac2dc77dd5edd7d2acb3ba78b6a8a3763421b766
zazila1 <zazila1@gmail.com>
2020-01-22 07:27:59 +0300

    Расчет радиуса для оверлапа перед размещением врага

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,13 +1,31 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
+using System.Collections;
 using UnityEngine;
-using UnityEngine.Experimental.PlayerLoop;
+using Random = UnityEngine.Random;
 
+[ExecuteInEditMode]
 public class Spawner : MonoBehaviour
 {
     [SerializeField] private PlayerController _Player;
     [SerializeField] private GameObject _EnemyPrefab;
-    public float _SpawnRadius;
+    [SerializeField] private float _SpawnOffset;
+    
+    private BoxCollider2D _EnemyCollider;
+    private float _UnitColliderRadius;
     
     
+    public float _SpawnerRadius;
+
+    private void Start()
+    {
+        _EnemyCollider = _EnemyPrefab.GetComponent<BoxCollider2D>();
+        _UnitColliderRadius = _SpawnOffset + (float)Math.Sqrt((Math.Pow(_EnemyCollider.size.x, 2) + (Math.Pow(_EnemyCollider.size.y, 2)))) / 2;
+        
+        StartCoroutine(InfiniteSpawn());
+    }
+
+    private IEnumerator InfiniteSpawn()
+    {
+        yield return new WaitForSeconds(Random.Range(0.1f, 3f));
+    }
 }
--- Assets/Scripts/SpawnerEditorHelper.cs
+++ Assets/Scripts/SpawnerEditorHelper.cs
@@ -14,15 +14,15 @@ public class SpawnerEditorHelper : MonoBehaviour
     {
         Gizmos.color = Color.white;
         float theta = 0;
-        float x = _Spawner._SpawnRadius * Mathf.Cos(theta);
-        float y = _Spawner._SpawnRadius * Mathf.Sin(theta);
+        float x = _Spawner._SpawnerRadius * Mathf.Cos(theta);
+        float y = _Spawner._SpawnerRadius * Mathf.Sin(theta);
         Vector3 pos = transform.position + new Vector3(x, 0, y);
         Vector3 newPos = pos;
         Vector3 lastPos = pos;
         for (theta = 0.1f; theta < Mathf.PI * 2; theta += 0.1f)
         {
-            x = _Spawner._SpawnRadius * Mathf.Cos(theta);
-            y = _Spawner._SpawnRadius * Mathf.Sin(theta);
+            x = _Spawner._SpawnerRadius * Mathf.Cos(theta);
+            y = _Spawner._SpawnerRadius * Mathf.Sin(theta);
             newPos = transform.position + new Vector3(x, y, 0);
             Gizmos.DrawLine(pos, newPos);
             pos = newPos;
