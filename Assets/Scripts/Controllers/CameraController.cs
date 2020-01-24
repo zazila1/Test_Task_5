eac411febf780c29d5171fd9c76c3ac23c9cca92
zazila1 <zazila1@gmail.com>
2020-01-24 21:34:13 +0300

    Мелкие правки

--- Assets/Scripts/Controllers/CameraController.cs
+++ Assets/Scripts/Controllers/CameraController.cs
@@ -1,15 +1,21 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
     [SerializeField] private GameObject _Player;
-    [SerializeField] private float _K_position = 0.4f;
-    [SerializeField] private float viewOffset = 0;
+    [SerializeField] private float _K_position = 0.01f;
+    [SerializeField] private float _ViewOffsetRange = 0.05f;
+    [SerializeField] private float _K_offset = 0.01f;
+    private Camera _Camera;
     private Transform _CamTransform;
     private Vector3 _PrevPos = Vector3.zero;
+    private Vector3 _PrevOffset = Vector3.zero;
 
+    private Vector3 _Cd;
     private void Awake()
     {
+        _Camera = Camera.main;
         _CamTransform = GetComponent<Transform>();
     }
 
@@ -18,14 +24,37 @@ public class CameraController : MonoBehaviour
     {
         MoveCameraToPlayer(_Player.transform.position);
     }
-    
+
+    private void FixedUpdate()
+    {
+        //CalculateNewCameraPosition();
+    }
+
     private void MoveCameraToPlayer(Vector3 playerPosition)
     {
-        var camPosition = _CamTransform.position;
+        // Наработки на будущее. Нужно сделать смещение камеры в зависимости от растояния до прицела
+        
+        //Vector3 mousePosition = _Camera.ScreenToWorldPoint(Input.mousePosition);
+        // Vector3 mousePosition = Input.mousePosition;
+        // Vector3 playerPosInScreenCoords = _Camera.WorldToScreenPoint(playerPosition);
+        // Vector3 viewOffset = mousePosition - playerPosInScreenCoords; //_Player.transform.up * _ViewOffsetRange;
+        // Debug.Log($"mousePosition {mousePosition} // playerPosInScreenCoords {playerPosInScreenCoords} " +
+        //           $"// viewOffset {viewOffset} // dist {Vector2.Distance(_Camera.ScreenToWorldPoint(mousePosition), playerPosition)} //");
+        //
+        // var w = _Camera.WorldToScreenPoint(Screen.width);
+        // viewOffset = new Vector3(Mathf.Clamp(viewOffset.x, Screen.width * 0.1f, Screen.width * 0.8f),
+        //     Mathf.Clamp(viewOffset.y, Screen.height * 0.1f, Screen.height * 0.8f), viewOffset.z);
+        //
+        // viewOffset = _Camera.ScreenToWorldPoint(viewOffset);
+        
+        Vector3 viewOffset = _Player.transform.up * _ViewOffsetRange;
+        Vector3 newViewOffset = _PrevOffset * (1f - _K_offset) + viewOffset * _K_offset;
+        _PrevOffset = newViewOffset;
+        
         Vector3 newPos = _PrevPos * (1f - _K_position) + playerPosition * _K_position;
-        _PrevPos = newPos + _Player.transform.up * viewOffset;
+        _PrevPos = newPos + newViewOffset;
 
-        camPosition = new Vector3(newPos.x, newPos.y, camPosition.z);
+        Vector3 camPosition = new Vector3(newPos.x, newPos.y, _CamTransform.position.z);
         _CamTransform.position = camPosition;
     }
 }
